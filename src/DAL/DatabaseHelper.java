2e52a8cf7007a4d7b68e52cff5d5d7d99460d3a8
KQuynhkewtie <dnkquynh2005@gmail.com>
2025-06-05 23:53:07 +0700

    upd

--- src/DAL/DatabaseHelper.java
+++ src/DAL/DatabaseHelper.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseHelper {
-	private static final String URL = "jdbc:oracle:thin:@//localhost:1521/orclpdb4";
-	private static final String USER = "TSISPharmacy";
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+	private static final String USER = "c##TSISPharmacy";
 	private static final String PASSWORD = "tsispharmacy";
 
 	// Khởi tạo driver
